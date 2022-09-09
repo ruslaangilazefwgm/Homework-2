33a5d82b1b2bc2dad4ffa28a7fe3a0d34f4267d6
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-09 19:48:27 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
-        dog = dog + 4;
-        System.out.println(dog);
-        cat = cat + 4;
-        System.out.println(cat);
-        paper = paper + 4;
-        System.out.println(paper);
-        dog = dog - 3.5;
-        System.out.println(dog);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        paper = paper - 7639;
-        System.out.println(paper);
-
+        // Задача 4
+        System.out.println("Задача 4");
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
@@ -26,6 +9,8 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
 
+        // Задача 5
+        System.out.println("Задача 5");
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
