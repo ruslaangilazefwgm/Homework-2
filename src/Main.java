e2e45d14a430606a70abd5f3ffaedf5ccd04bbef
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-09 18:44:24 +0300

    Задание 2 выполнено (задачи 4 и 5).

--- src/Main.java
+++ src/Main.java
@@ -18,12 +18,14 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
