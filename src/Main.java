296025599242ab8ddbca13a0aeb30892b2166767
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-09 19:17:31 +0300

    Задание 3 выполнено (задачи 6,7,8).

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         paper = paper - 7639;
         System.out.println(paper);
 
+
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
@@ -34,5 +35,30 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+
+
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес двух бойцов " + totalWeight + " кг");
+
+        var weightDifferent = firstBoxerWeight - secondBoxerWeight;
+        System.out.println("Разница между весами двух бойцов " + weightDifferent + " кг");
+
+        var weightDifferent1 = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница между весами двух бойцов " + weightDifferent1 + " кг");
+
+        var weightDifferent2 = secondBoxerWeight % firstBoxerWeight;
+        System.out.println("Разница между весами двух бойцов " + weightDifferent2 + " кг");
+
+        var totalWorkHours = 640;
+        var hoursEveryWorker = 8;
+        var totalWorkers = totalWorkHours / hoursEveryWorker;
+        System.out.println("Всего работников в компании - " + totalWorkers + " человек");
+
+        var newTotalWorkers = totalWorkers + 94;
+        var newHoursEveryWorker = totalWorkHours / newTotalWorkers;
+        System.out.println("Если в компании работает " + newTotalWorkers + " человек, то всего " + newHoursEveryWorker + " часа работы может быть поделено между сотрудниками");
+
     }
 }
